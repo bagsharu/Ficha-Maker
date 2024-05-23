32d30d782890173c2bccffe00192291a4202ff0f
Haru <gabs.haru@gmail.com>
2024-05-22 20:09:46 -0400

    Nova classe
    Implementação da leitura de dados movida para nova classe.

---
+++ src/Ordem/LeituraDados.java
@@ -0,0 +1,30 @@
+package Ordem;
+
+import javax.swing.JOptionPane;
+
+public class LeituraDados {
+    public static void LerDados(int classe, int NEX, int[] atributos){
+        switch (classe) {
+			case 0:
+				Combatente combatente = new Combatente(NEX, atributos);
+				JOptionPane.showMessageDialog(null, 
+															combatente.toString(), 
+															"Agente Ordo Realitas", 
+															JOptionPane.PLAIN_MESSAGE);
+				break;
+			case 1:
+				Especialista especialista = new Especialista(NEX, atributos);
+				JOptionPane.showMessageDialog(null, 
+															especialista.toString(), 
+															"Agente Ordo Realitas", 
+															JOptionPane.PLAIN_MESSAGE);
+				break;
+			case 3:
+				Ocultista ocultista = new Ocultista(NEX, atributos);
+				JOptionPane.showMessageDialog(null, ocultista.toString(), 
+																	"Agente Ordo Realitas", 
+																	JOptionPane.PLAIN_MESSAGE);				
+				break;
+		}
+    }
+}
