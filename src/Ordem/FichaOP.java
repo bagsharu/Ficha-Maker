17d9f5d40c4be402a8259c2760be142949e167b8
Haru <123333280+bagsharu@users.noreply.github.com>
2024-05-23 17:14:26 -0400

    Atualização na formatação

--- src/Ordem/FichaOP.java
+++ src/Ordem/FichaOP.java
@@ -10,20 +10,19 @@ public class FichaOP {
 		String[] options = {"Combatente","Especialista","Ocultista"};
 
 		// Dispõe ao usuário as três classes disponíveis em Ordem Paranormal.
-		int classe = JOptionPane.showOptionDialog(null, 
-											"Selecione uma das Classes:",
-												"Agente Ordo Realitas",  
-													JOptionPane.DEFAULT_OPTION, 
-													JOptionPane.INFORMATION_MESSAGE,
-													null, options, options[0]);
+		int classe = JOptionPane.showOptionDialog(null,"Selecione uma das Classes:",
+								"Agente Ordo Realitas",  
+								JOptionPane.DEFAULT_OPTION, 
+								JOptionPane.INFORMATION_MESSAGE,
+								null, options, options[0]);
 		
 		// Recebe o NEX pela interface de usuário e converte para int.
 		String nivel = JOptionPane.showInputDialog("Digite o NEX do agente:");
 		int NEX = Integer.parseInt(nivel);
 		
 		// Recebe os atributos de ficha e converte para valores inteiros em values.
-		String tokens2 = JOptionPane.showInputDialog("Agora, insira os atributos do agente na seguinte ordem\n" + 
-													 "- Agilidade, Força, Intelecto, Presença e Vigor:");
+		String tokens2 = JOptionPane.showInputDialog("Agora, insira os atributos do agente na seguinte ordem:\n" + 
+								"- Agilidade, Força, Intelecto, Presença e Vigor:");
 		String[] values = tokens2.split(" ");
         for (int i = 0; i < 5; i++) {
             	atributos[i] = Integer.parseInt(values[i]);
